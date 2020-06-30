993a8085b1a4cf9c1db443cb3642915ecb293847
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-06-30 14:58:21 -0400

    eliminated bootstrap from import

--- article-app/src/index.js
+++ article-app/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import CheckedDropDown from './components/checkedDropDown'
-import "bootstrap/dist/css/bootstrap.css"
 
 ReactDOM.render(
   <React.StrictMode>
