65e00e8ccf5a56e902568ad81c312046d7f1aec7
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-06-30 20:11:15 -0400

    added formatting for search button

--- article-app/src/components/dashboard.jsx
+++ article-app/src/components/dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import CheckedDropDown from './checkedDropDown'
 import TextBarDate from './textBarDate'
+import './css/textBarDate.css'
 import './css/dashboard.css'
 
 class Dashboard extends React.Component {
@@ -19,27 +20,49 @@ class Dashboard extends React.Component {
                     <thead></thead>
                     <tbody>
                     <tr>
+                        <td className="searchBarCell" colSpan="6">
+                            <input className="searchBar" type="text"></input>
+                        </td>
+                    </tr>
+                    <tr>
                         <td>
                         <CheckedDropDown
                                 url="https://article-search-api.herokuapp.com/api/countryList"
                                 name="Countries"/>
                         </td>
                         <td>
-                        <TextBarDate />
+                        <input className="dateText" type="text"></input>
+                        <p>Example:</p>
+                        <p>Date: Jan 01 2020</p>
+                        <p>Input: 01/01/2020</p>
                         </td>
                         <td>
-                        <TextBarDate />
+                        <input className="dateText" type="text"></input>
+                        <p>Example:</p>
+                        <p>Time: 1:30 PM</p>
+                        <p>Input: 13:00:00</p>
                         </td>
                         <td>
-                        <TextBarDate />
+                        <input className="dateText" type="text"></input>
+                        <p>Example:</p>
+                        <p>Date: Jan 03 2020</p>
+                        <p>Input: 01/03/2020</p>
                         </td>
                         <td>
-                        <TextBarDate />
+                        <input className="dateText" type="text"></input>
+                        <p>Example:</p>
+                        <p>Time: 5:14 AM</p>
+                        <p>Input: 05:14:00</p>
                         </td>
                         <td>
                         <CheckedDropDown
                                 url="https://article-search-api.herokuapp.com/api/outputList"
                                 name="Output Types"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="searchButtonCell" colSpan="6">
+                            <button id="searchButton">Search</button>
                         </td>
                     </tr>
                     </tbody>    
